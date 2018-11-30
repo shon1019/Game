b3925a6dfbf8396f64e318b38f56db1c26a94feb
shion1019 <shion10193@gmail.com>
2018-11-30 12:57:35 +0800

    add music

---
+++ Assets/Script/Sound/MusicManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] Clip;
+    public AudioSource Music;
+
+    public void PlayMusic(int _music)
+    {
+        if (Clip.Length > _music)
+        {
+            Debug.Log("Play Music");
+            Music.clip = Clip[_music];
+            Music.Play();
+        }
+    }
+}
---
+++ Assets/Script/Sound/SoundEffectManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectManager : MonoBehaviour {
+
+    public AudioClip[] audioClips;
+
+    public enum EffectName
+    {
+        Dead,
+        Collision,
+        TimeUP,
+        Champion,
+        Limit
+    }
+
+    public void PlaySoundEffect(EffectName _effect,Vector3 _pos)
+    {
+
+        switch (_effect)
+        {
+            case EffectName.Dead:
+                AudioSource.PlayClipAtPoint(audioClips[0], _pos);
+                break;
+            case EffectName.Collision:
+                AudioSource.PlayClipAtPoint(audioClips[1], _pos);
+                break;
+            case EffectName.TimeUP:
+                AudioSource.PlayClipAtPoint(audioClips[2], _pos);
+                break;
+            case EffectName.Champion:
+                AudioSource.PlayClipAtPoint(audioClips[3], _pos);
+                break;
+            case EffectName.Limit:
+                AudioSource.PlayClipAtPoint(audioClips[4], _pos);
+                break;
+        }
+    }
+
+    public void Stop(EffectName _effect)
+    {
+
+    }
+
+    public void StopAll()
+    {
+        
+    }
+}
