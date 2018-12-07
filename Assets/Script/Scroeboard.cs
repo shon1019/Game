ed84759bb885f3e2cafe24295a66af0fecc353cb
WeiZero <cd232310@gmail.com>
2018-12-07 21:44:56 +0800

    UI

--- Assets/Script/Scroeboard.cs
+++ Assets/Script/Scroeboard.cs
@@ -5,26 +5,44 @@ using UnityEngine.UI;
 
 public class Scroeboard : MonoBehaviour {
 
-    public GameObject _Player1;
-    public Text _P1Score;
-    public float Speed;
+    public Image[] _Player = new Image[4];  // 積分條
+    public Text[] _Score = new Text[4];     // 分數面板
+    public float Speed;                     // 積分條上升速度
+    private float[] score = new float[4];   // 分數
+    private float highScore = 0;            // 最高分
 
-    float score;
-    // Use this for initialization
     void Start () {
 		
 	}
-
+    /**
+     * 設定分數
+     */
     void Awake()
     {
-        string Score = _P1Score.GetComponent<Text>().text;
-        score = float.Parse(Score) / 100;
+        for (int i = 0; i < 4; i++) {
+            string Score = _Score[i].text;
+            score[i] = float.Parse(Score);
+            if (highScore < score[i])
+                highScore = score[i];
+        }
+        NormalizeScore();
+    }
+    /**
+     * 簡易正規化
+     */
+    private void NormalizeScore() {
+        for (int i = 0; i < 4; i++) {
+            score[i] /= highScore;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_Player1.GetComponent<Image>().fillAmount < score)
-            _Player1.GetComponent<Image>().fillAmount += Speed;
+        for (int i = 0; i < 4; i++)
+        {
+            if (_Player[i].fillAmount < score[i])
+                _Player[i].fillAmount += Speed;
+        }
     }
 }
