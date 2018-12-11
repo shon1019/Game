06237048304167765722bd5b076affdb31c67af5
WeiZero <cd232310@gmail.com>
2018-12-11 14:09:14 +0800

    update

---
+++ Assets/Scripts/HallCtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * 簡易大廳轉換按鈕
+ */
+public class HallCtrl : MonoBehaviour {
+
+    public void Btn_Start() {
+        SceneManager.LoadScene(1);
+    }
+}
